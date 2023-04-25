6152a4444f926e95d92c3af856e6e0c1b600690d
sharull9 <sharullmusicplaylist@gmail.com>
2023-04-25 22:26:41 +0530

    redirect added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/new",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/popular",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/trending",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/categories",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
